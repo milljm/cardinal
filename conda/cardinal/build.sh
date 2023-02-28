0be0bea98b3d9e624a50e6def76c1d8b19958fc6
Jason M Miller <Jason.Miller@inl.gov>
2023-02-28 13:25:23 -0700

    Enable DAGMC
    A version to have everything linked
    Add a test for each contrib included
    
    Refs: 594

--- conda/cardinal/build.sh
+++ conda/cardinal/build.sh
@@ -8,7 +8,7 @@ export NEKRS_HOME=${CONTRIB_INSTALL_DIR}
 export HDF5_ROOT=${PETSC_DIR}
 export MOOSE_SKIP_DOCS=True
 export METHOD=opt
-
+export ENABLE_DAGMC=yes
 # gslib inside nekRS contrib: has some sort of issue with Condas PREFIX
 export OLD_PREFIX=${PREFIX}
 unset PREFIX
@@ -31,8 +31,14 @@ cp scripts/download-test-suite-cross-sections.sh ${PREFIX}/cardinal/bin/
 mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/etc/conda/deactivate.d"
 cat <<EOF > "${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
 export PATH=\${PATH}:${PREFIX}/cardinal/bin:${PREFIX}/cardinal/contrib/bin
+export NEKRS_HOME=${PREFIX}/cardinal/contrib
+export DAGMC_DIR=${PREFIX}/cardinal/contrib
+export MOAB_DIR=${PREFIX}/cardinal/contrib
 EOF
 cat <<EOF > "${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
 export PATH=\${PATH%":${PREFIX}/cardinal/bin"}
 export PATH=\${PATH%":${PREFIX}/cardinal/contrib/bin"}
+unset NEKRS_HOME
+unset DAGMC_DIR
+unset MOAB_DIR
 EOF
